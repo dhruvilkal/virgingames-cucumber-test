123fcea85bbd0685f02c63e9db4f4ac2f0479ad1
dhruv <dhruvilkalathiya04@gmail.com>
2024-02-02 19:35:57 +0000

    Initial commit

---
+++ src/test/java/com/virgingames/steps/HomePageSteps.java
@@ -0,0 +1,35 @@
+package com.virgingames.steps;
+
+import com.virgingames.pages.HomePage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class HomePageSteps {
+    @Given("I am on home page")
+    public void iAmOnHomePage() {
+    }
+
+    @And("I verify I am on home page")
+    public void iVerifyIAmOnHomePage() {
+        String expectedTitle = "Virgin Games | Play Online Betting Games With Real Money";
+        Assert.assertEquals(new HomePage().getTitle(), expectedTitle);
+
+    }
+
+    @And("I accept Necessary cookies only")
+    public void iAcceptNecessaryCookiesOnly() {
+        new HomePage().clickOnNecessaryCookiesOnly();
+    }
+
+    @And("I click on Join Now tab")
+    public void iClickOnJoinNowTab() {
+        new HomePage().clickOnJoinNowTab();
+    }
+
+    @When("I click on {string} tab")
+    public void iClickOnOnlineSlotsTab(String menu) {
+        new HomePage().selectTopMenu(menu);
+    }
+}
---
+++ src/test/resources/features/onlineslots.feature
@@ -0,0 +1,12 @@
+Feature: Online Slots page features
+
+  As a user I want to verify user should navigate to Online slots features
+
+  Scenario: user should navigate to Online Slots page successfully
+    Given I am on home page
+    And I verify I am on home page
+    And I accept Necessary cookies only
+    When I click on 'Online Slots' tab
+    Then I verify text 'Play Online Slots at Virgin Games'
+
+
